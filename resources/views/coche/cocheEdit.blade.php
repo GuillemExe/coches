50e5eefb80e68f9490805adf9007e63470191775
GuillemExe <guillem.arrojo@mataro.epiaedu.cat>
2020-03-02 15:08:54 +0100

    fix

--- resources/views/coche/cocheEdit.blade.php
+++ resources/views/coche/cocheEdit.blade.php
@@ -9,7 +9,24 @@
 
 @section('content')
 <div>
-    HOLA QUE TAL
+    <form>
+        <div class="form-group">
+            <label>Make</label>
+            <input type="text" class="form-control" placeholder="Make">
+        </div>
+        <div class="form-group">
+            <label>Model</label>
+            <input type="text" class="form-control" placeholder="Make">
+        </div>
+        <div class="form-group">
+            <label>Produced on</label>
+            <input type="date" class="form-control" placeholder="Make">
+        </div>
+        <div class="form-group">
+            <label>Coche image</label>
+            <input type="text" class="form-control" placeholder="Make">
+        </div>
+    </form>
 </div>
 
 @endsection
